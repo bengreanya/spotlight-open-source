d3af42852765015f257cd7941bf9f68fd66049af
Ben Greanya <bengreanya@gmail.com>
2023-01-31 11:52:04 -0800

    chart is rendering

--- src/Charts.js
+++ src/Charts.js
@@ -1,5 +1,15 @@
 import React from 'react';
-
+import { VictoryChart, VictoryBar } from 'victory';
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
 export default function Charts() {
-  return <div>Charts</div>;
+  return (
+    <VictoryChart>
+      <VictoryBar data={data} x="quarter" y="earnings" />
+    </VictoryChart>
+  );
 }
